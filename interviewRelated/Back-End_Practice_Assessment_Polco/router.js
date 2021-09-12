6849726288d5359905f5478940be97b6a0c296e1
Stephanie Moore <smoore_forestry@outlook.com>
2021-09-12 15:03:30 -0400

    router created for GET namesand methodNotAllowed error created

---
+++ interviewRelated/Back-End_Practice_Assessment_Polco/router.js
@@ -0,0 +1,8 @@
+const router = require("express").Router();
+const controller = require("./controller");
+const methodNotAllowed = require("./errors/methodNotAllowed");
+
+router
+  .route("/recipes")
+  .get(controller.listNames)
+  .all(methodNotAllowed);
