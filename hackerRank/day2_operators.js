2e89a6be8934af838667e8352a25096e4692860c
Stephanie Moore <smoore_forestry@outlook.com>
2021-09-12 22:50:37 -0400

    hackerRank challenge

---
+++ hackerRank/day2_operators.js
@@ -0,0 +1,82 @@
+/*
+Objective
+In this challenge, you will work with arithmetic operators. Check out the Tutorial tab for learning materials and an instructional video.
+
+Task
+Given the meal price (base cost of a meal), tip percent (the percentage of the meal price being added as tip), and tax percent (the percentage of the meal price being added as tax) for a meal, find and print the meal's total cost. Round the result to the nearest integer.
+
+Example
+meal_cost = 100;
+tip_percent = 15;
+tax_percent = 8
+
+A tip of 15% * 100 = 15, and the taxes are 8% * 100 = 8. Print the value 123 and return from the function.
+
+Function Description
+Complete the solve function in the editor below.
+
+solve has the following parameters:
+
+int meal_cost: the cost of food before tip and tax
+int tip_percent: the tip percentage
+int tax_percent: the tax percentage
+Returns The function returns nothing. Print the calculated value, rounded to the nearest integer.
+
+Note: Be sure to use precise values for your calculations, or you may end up with an incorrectly rounded result.
+
+Input Format
+
+There are  lines of numeric input:
+The first line has a double,  (the cost of the meal before tax and tip).
+The second line has an integer,  (the percentage of  being added as tip).
+The third line has an integer,  (the percentage of  being added as tax).
+*/
+
+'use strict';
+
+process.stdin.resume();
+process.stdin.setEncoding('utf-8');
+
+let inputString = '';
+let currentLine = 0;
+
+process.stdin.on('data', function (inputStdin) {
+  inputString += inputStdin;
+});
+
+process.stdin.on('end', function () {
+  inputString = inputString.split('\n');
+
+  main();
+});
+
+function readLine() {
+  return inputString[currentLine++];
+}
+
+/*
+ * Complete the 'solve' function below.
+ *
+ * The function accepts following parameters:
+ *  1. DOUBLE meal_cost
+ *  2. INTEGER tip_percent
+ *  3. INTEGER tax_percent
+ */
+
+function solve(meal_cost, tip_percent, tax_percent) {
+  // Write your code here
+  const tip = (tip_percent / 100) * meal_cost;
+  const tax = (tax_percent / 100) * meal_cost;
+  const total = Math.round(meal_cost + tip + tax);
+  console.log(total);
+}
+
+function main() {
+  const meal_cost = parseFloat(readLine().trim());
+
+  const tip_percent = parseInt(readLine().trim(), 10);
+
+  const tax_percent = parseInt(readLine().trim(), 10);
+
+  solve(meal_cost, tip_percent, tax_percent);
+}
