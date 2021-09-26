8b431f9f883f5879f58fbd52174a0298ba8c69af
Stephanie Moore <smoore_forestry@outlook.com>
2021-09-25 21:19:28 -0400

    update

--- interviewQuestions/non-repeatingLetter.js
+++ interviewQuestions/non-repeatingLetter.js
@@ -1,9 +1,9 @@
 // Return the first non-repeating letter, there will be one
 
 function nonRepeatingLetter(str) {
-  for (let i = 0; i < str.length; i++) {
-    let character = str[i];
-    if (str.indexOf(character) == i && str.indexOf(character, i + 1) == -1) {
+  for (let index = 0; index < str.length; index++) {
+    let character = str[index];
+    if (str.indexOf(character) == index && str.indexOf(character, index + 1) == -1) {
       return character;
     }
   }
